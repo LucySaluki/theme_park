d2678c219b014aeb522da2974712fa5b299bcf51
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-09-28 20:48:56 +0100

    intital commit and playground and tobaccoStall tests

---
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -0,0 +1,46 @@
+package stalls;
+
+import org.junit.Before;
+import org.junit.Test;
+import people.Visitor;
+
+import static org.junit.Assert.assertEquals;
+
+public class TobaccoStallTest {
+
+    TobaccoStall tobaccoStall;
+    Visitor visitor1;
+    Visitor visitor2;
+
+    @Before
+    public void setUp() throws Exception {
+        tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        visitor1= new Visitor(19,180, 20.00);
+        visitor2= new Visitor(16,150, 5.00);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Jacks Drum", tobaccoStall.getName());
+    }
+
+    @Test
+    public void hasOwner() {
+        assertEquals("Jack Jarvis", tobaccoStall.getOwnerName());
+    }
+
+    @Test
+    public void hasParkingSpot() {
+        assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void  adultCanBuy(){
+        assertEquals(true, tobaccoStall.isAllowed(visitor1));
+    }
+
+    @Test
+    public void  childCantBuy(){
+        assertEquals(false, tobaccoStall.isAllowed(visitor2));
+    }
+}
